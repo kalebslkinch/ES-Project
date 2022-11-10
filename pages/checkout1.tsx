f1a0500556626af8b3d98f501c7fc43cd8d7358c
kalebslkinch <kalebslkinch@gmail.com>
2022-11-10 23:57:55 +0000

    Added types to Billing Page

--- pages/checkout1.tsx
+++ pages/checkout1.tsx
@@ -4,11 +4,12 @@ import CheckoutLayout from '../components/checkout/CheckoutLayout'
 const Checkout: NextPage = () => {
 	return (
 		<div className="max-w-screen w-full animate-fadein rounded-md sm:px-8 ">
+			{/* Title */}
 			<div className="flex justify-center sm:max-w-7xl">
-				{/* Title */}
 				<h1 className="mt-10 text-5xl font-medium text-gray-800">Shopping Bag</h1>
 			</div>
 
+			{/* Checkout Layout */}
 			<CheckoutLayout />
 		</div>
 	)
--- utils/types/customerinfo.ts
+++ utils/types/customerinfo.ts
@@ -1,8 +1,1 @@
-export interface CustomerInfo {
-  firstName: string;
-  surName: string;
-  address?: string;
-  number?: string;
-  postCode?: string;
-  customerEmail: string;
-}
+export interface CustomerInfo {}
