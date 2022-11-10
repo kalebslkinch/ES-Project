847d42eefe3493a72209f2f0c18e033df0ab552e
kalebslkinch <kalebslkinch@gmail.com>
2022-11-10 22:34:50 +0000

    Created types for checkout page and child components

--- components/checkout/CheckoutItems.tsx
+++ components/checkout/CheckoutItems.tsx
@@ -1,164 +1,186 @@
-import { useRouter } from 'next/router';
-import cookie from 'js-cookie';
-import Plus from '../svg/Plus';
-import Minus from '../svg/Minus';
-import Link from 'next/link';
-const CheckoutItems = ({ title, description, price, image, quantity, id }) => {
-  // Router
-  const router = useRouter();
+import { useRouter } from 'next/router'
+import cookie from 'js-cookie'
+import Plus from '../svg/Plus'
+import Minus from '../svg/Minus'
+import Link from 'next/link'
+import { FC } from 'react'
+import { StripeIssuingCardCvcDisplayElement } from '@stripe/stripe-js'
+const CheckoutItems: FC<{
+	id: string
+	title: string
+	description: string
+	image: string
+	price: number | string
+	quantity: number
+}> = ({ id, title, description, image, price, quantity }) => {
+	// Router
+	const router = useRouter()
 
-  // Refresh Router
-  const refreshData = () => {
-    router.replace(router.asPath);
-  };
+	// Refresh Router
+	const refreshData = (): void => {
+		router.replace(router.asPath)
+	}
 
-  // Put into object
-  const items = {
-    id,
-    title,
-    description,
-    image,
-    price,
-    quantity,
-  };
+	// Put into object
+	const items: {
+		id: string
+		title: string
+		description: string
+		image: string
+		price: number | string
+		quantity: number
+	} = {
+		id,
+		title,
+		description,
+		image,
+		price,
+		quantity
+	}
 
-  const newPrice = quantity * price;
+	const newPrice: number = quantity * (price as number)
 
-  const cart = 'cart';
-  // Get items from the local storage
-  const stringGetItems = cookie.get(cart);
-  const getItems = JSON.parse(cookie.get(cart));
+	const cart: string = 'cart'
 
-  // Get the tiltes from the array
-  const titles = getItems.map((data) => data.title);
+	// Get items from the local storage
+	const stringGetItems = cookie.get(cart)
+	const getItems: {
+		id: string
+		title: string
+		description: string
+		image: string
+		price: number | string
+		quantity: number
+	}[] = JSON.parse(cookie.get(cart))
 
-  // Index of the items
-  const index = titles.indexOf(title);
+	// Get the tiltes from the array
+	const titles: string[] = getItems.map(data => data.title)
 
-  // For removing from an Array
-  function arrayRemove(arr, value) {
-    return arr.filter(function (ele) {
-      return ele != value;
-    });
-  }
+	// Index of the items
+	const index: number = titles.indexOf(title)
 
-  // Remove from Cart
-  const handleRemove = () => {
-    const newCart = arrayRemove(getItems, getItems[index]);
+	// For removing from an Array
+	function arrayRemove(arr: {}[], value: {}) {
+		return arr.filter(function (ele: {}) {
+			return ele != value
+		})
+	}
 
-    // Add back to the local storage
-    cookie.set(cart, newCart);
+	// Remove from Cart
+	const handleRemove = (): void => {
+		const newCart = arrayRemove(getItems, getItems[index])
 
-    refreshData();
-  };
+		// Add back to the local storage
+		cookie.set(cart, newCart)
 
-  // Increase quantity amount
-  const handleIncrease = () => {
-    const newCart = stringGetItems.replace(
-      JSON.stringify(items),
-      JSON.stringify({ ...items, quantity: quantity + 1 })
-    );
+		refreshData()
+	}
 
-    cookie.set(cart, JSON.parse(newCart));
-    refreshData();
-  };
+	// Increase quantity amount
+	const handleIncrease = (): void => {
+		const newCart = stringGetItems.replace(JSON.stringify(items), JSON.stringify({ ...items, quantity: quantity + 1 }))
 
-  // Decrease quantity amount
-  const handleDecrease = () => {
-    const newCart = stringGetItems.replace(
-      JSON.stringify(items),
-      JSON.stringify({ ...items, quantity: quantity - 1 })
-    );
-    cookie.set(cart, newCart);
-    refreshData();
-  };
+		cookie.set(cart, JSON.parse(newCart))
+		refreshData()
+	}
 
-  // When quantity is less than 1 it removes it from the local strorage
-  if (quantity < 1) {
-    handleRemove();
-  }
+	// Decrease quantity amount
+	const handleDecrease = (): void => {
+		const newCart = stringGetItems.replace(JSON.stringify(items), JSON.stringify({ ...items, quantity: quantity - 1 }))
+		cookie.set(cart, newCart)
+		refreshData()
+	}
 
-  return (
-    <>
-      <tbody>
-        <tr>
-          <td className='  border-b border-gray-200 bg-white px-5  py-5 text-sm'>
-            <div className='flex items-center'>
-              <div className='flex-shrink-0'>
-                <Link href={`/${id}`}>
-                  <a className='relative block'>
-                    <img
-                      alt='profile'
-                      src={image}
-                      className='mx-auto h-10  w-10 object-cover '
-                    />
-                  </a>
-                </Link>
-              </div>
-              <div className='ml-3'>
-                <p className='whitespace-no-wrap overflow-hidden truncate text-gray-900'>
-                  {title}
-                </p>
-              </div>
-            </div>
-          </td>
-          <td className='border-b border-gray-200 bg-white px-5 py-5 text-sm'>
-            <p className='whitespace-no-wrap overflow-hidden truncate text-gray-900'>
-              {description}
-            </p>
-          </td>
-          <td className='border-b  border-gray-200 bg-white px-5 py-5  text-sm'>
-            <p className='whitespace-no-wrap text-gray-900'>
-              {' '}
-              {price >= 1 ? (
-                <> £{parseFloat(price).toFixed(2)} </>
-              ) : (
-                <>{parseFloat(price).toFixed(2).toString().slice(-2)}p</>
-              )}
-            </p>
-          </td>
+	// When quantity is less than 1 it removes it from the local strorage
+	if (quantity < 1) {
+		handleRemove()
+	}
 
-          <td className='border-b border-gray-200 bg-white px-5 py-5 text-sm'>
-            <p className='flex justify-center text-gray-900 '>{quantity}</p>
-          </td>
+	return (
+		<>
+			<tbody>
+				<tr>
+					<td className="  border-b border-gray-200 bg-white px-5  py-5 text-sm">
+						<div className="flex items-center">
+							{/* Product Link */}
+							<div className="flex-shrink-0">
+								<Link href={`/${id}`}>
+									<a className="relative block">
+										{/* Product Image */}
+										<img alt="profile" src={image} className="mx-auto h-10  w-10 object-cover " />
+									</a>
+								</Link>
+							</div>
 
-          <td className='animate-quantityfadein flex border-b border-gray-200 bg-white  px-5 py-8 '>
-            <button
-              className='pl-1 focus:outline-none'
-              onClick={handleDecrease}
-            >
-              <Minus className='h-4 w-4' />
-            </button>
-            <button
-              className='pl-1 focus:outline-none'
-              onClick={handleIncrease}
-            >
-              <Plus className='h-4 w-4' />
-            </button>
-          </td>
-          <td className='border-b border-gray-200 bg-white px-5 py-5 text-sm'>
-            <p className='whitespace-no-wrap flex justify-center text-gray-900'>
-              {newPrice >= 1 ? (
-                <> £{newPrice.toFixed(2)} </>
-              ) : (
-                <>{newPrice.toFixed(2).toString().slice(-2)}p</>
-              )}
-            </p>
-          </td>
-          <td className='border-b border-gray-200  bg-white px-5 py-5 text-sm'>
-            <span className='static   px-3 py-1 font-semibold leading-tight text-green-900'>
-              <button
-                onClick={handleRemove}
-                className='transform rounded-full bg-gray-300 px-2 py-2 text-red-800 transition delay-100 duration-300 hover:bg-gray-400 focus:outline-none'
-              >
-                Remove
-              </button>
-            </span>
-          </td>
-        </tr>
-      </tbody>
-    </>
-  );
-};
+							{/* Product Title */}
+							<div className="ml-3">
+								<p className="whitespace-no-wrap overflow-hidden truncate text-gray-900">{title}</p>
+							</div>
+						</div>
+					</td>
+					{/* Product Description */}
+					<td className="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+						<p className="whitespace-no-wrap overflow-hidden truncate text-gray-900">{description}</p>
+					</td>
 
-export default CheckoutItems;
+					{/* Product Price */}
+					<td className="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+						<p className="whitespace-no-wrap text-gray-900">
+							{' '}
+							{price >= 1 ? (
+								<> £{parseFloat(price as string).toFixed(2)} </>
+							) : (
+								<>
+									{parseFloat(price as string)
+										.toFixed(2)
+										.toString()
+										.slice(-2)}
+									p
+								</>
+							)}
+						</p>
+					</td>
+
+					{/* Product Quantity */}
+					<td className="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+						<p className="flex justify-center text-gray-900 ">{quantity}</p>
+					</td>
+
+					{/* Quantity Toggle Buttons */}
+					<td className="animate-quantityfadein flex border-b border-gray-200 bg-white  px-5 py-8 ">
+						{/* Minus Button */}
+						<button className="pl-1 focus:outline-none" onClick={handleDecrease}>
+							<Minus className="h-4 w-4" />
+						</button>
+
+						{/* Plus Button */}
+						<button className="pl-1 focus:outline-none" onClick={handleIncrease}>
+							<Plus className="h-4 w-4" />
+						</button>
+					</td>
+
+					{/* New Calculated Price */}
+					<td className="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+						<p className="whitespace-no-wrap flex justify-center text-gray-900">
+							{newPrice >= 1 ? <> £{newPrice.toFixed(2)} </> : <>{newPrice.toFixed(2).toString().slice(-2)}p</>}
+						</p>
+					</td>
+
+					{/* Product Remove Button */}
+					<td className="border-b border-gray-200  bg-white px-5 py-5 text-sm">
+						<span className="static   px-3 py-1 font-semibold leading-tight text-green-900">
+							<button
+								onClick={handleRemove}
+								className="transform rounded-full bg-gray-300 px-2 py-2 text-red-800 transition delay-100 duration-300 hover:bg-gray-400 focus:outline-none"
+							>
+								Remove
+							</button>
+						</span>
+					</td>
+				</tr>
+			</tbody>
+		</>
+	)
+}
+
+export default CheckoutItems
--- pages/checkout1.tsx
+++ pages/checkout1.tsx
@@ -1,16 +1,17 @@
-import CheckoutLayout from '../components/checkout/CheckoutLayout';
+import { NextPage } from 'next'
+import CheckoutLayout from '../components/checkout/CheckoutLayout'
 
-const Checkout = () => {
-  return (
-    <div className='max-w-screen w-full animate-fadein rounded-md sm:px-8 '>
-      <div className='flex justify-center sm:max-w-7xl'>
-        <h1 className='mt-10 text-5xl font-medium text-gray-800'>
-          Shopping Bag
-        </h1>
-      </div>
-      <CheckoutLayout />
-    </div>
-  );
-};
+const Checkout: NextPage = () => {
+	return (
+		<div className="max-w-screen w-full animate-fadein rounded-md sm:px-8 ">
+			<div className="flex justify-center sm:max-w-7xl">
+				{/* Title */}
+				<h1 className="mt-10 text-5xl font-medium text-gray-800">Shopping Bag</h1>
+			</div>
 
-export default Checkout;
+			<CheckoutLayout />
+		</div>
+	)
+}
+
+export default Checkout
