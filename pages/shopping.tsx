188a894b2be8b7cdfe809e808bdf057ab792464a
kalebslkinch <kalebslkinch@gmail.com>
2022-11-11 03:24:25 +0000

    Replaced apollo with prisma for product data

--- pages/shopping.tsx
+++ pages/shopping.tsx
@@ -1,46 +1,31 @@
 import React from 'react'
 import Products from '../components/products/Products'
-import Loading from '../components/Loading'
-import GET_PRODUCTS_AVAILABLE from '../graphql/querys/getProductsAvailable'
-import { useQuery } from '@apollo/client'
 import { FSCol } from '../components/EasyComponents/FScreen'
+import prisma from '../lib/prisma'
+import { NextPage } from 'next'
 
-const Shopping = () => {
-	// Query Data
-	const { data: getProductsAvaialable, loading: loadingProductsAvaialable, error } = useQuery(GET_PRODUCTS_AVAILABLE)
-
-	if (error) return `Error ${error}`
-	if (loadingProductsAvaialable) {
-		return <Loading />
-	}
-	// Deconstruct Data
-	const productsAvailable = (
-		getProductsAvaialable as {
-			allProductsAvailable: {
-				data: {
-					_id: string
-					price: string
-					image: string
-					description: string
-					title: string
-				}[]
-			}
-		}
-	).allProductsAvailable.data
-
+const Shopping: NextPage<{
+	products: {
+		id: string
+		title: string
+		description: string
+		image: string
+		price: number | string
+	}[]
+}> = ({ products }) => {
 	return (
 		<FSCol className="bg-[#f7f7f5]">
 			{/* Title */}
 			<h1 className="mt-8 flex  justify-center py-4 text-6xl font-medium text-gray-800 sm:text-5xl">Products</h1>
 			{/* Products */}
 			<FSCol className="mx-auto mt-5 gap-x-3 px-6 sm:grid sm:w-11/12 sm:grid-cols-2 md:grid-cols-3 lg:w-[95%] lg:grid-cols-4 xl:max-w-[1200px] xl:px-0">
-				{productsAvailable.map(data => (
+				{products.map(data => (
 					<Products
 						title={data.title}
 						description={data.description}
 						image={data.image}
-						price={data.price}
-						id={data._id}
+						price={data.price as string}
+						id={data.id}
 					/>
 				))}
 			</FSCol>
@@ -49,3 +34,12 @@ const Shopping = () => {
 }
 
 export default Shopping
+
+export const getStaticProps = async () => {
+	const products = await prisma.products.findMany()
+	return {
+		props: {
+			products
+		}
+	}
+}
