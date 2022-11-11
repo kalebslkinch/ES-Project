9b47c59e85889b18234fef7b3827074b92b00ac0
kalebslkinch <kalebslkinch@gmail.com>
2022-11-11 03:39:46 +0000

    Replaced apolloclient with prisma for Testimonials data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,52 +1,58 @@
-import React, { useEffect } from 'react';
-import cookie from 'js-cookie';
-import ALL_INSTAGRAM from '../graphql/querys/getAllInstagram';
-import { useQuery } from '@apollo/client';
-import Loading from '../components/Loading';
-import { useInView } from 'react-intersection-observer';
-import dynamic from 'next/dynamic';
-import  Header  from './../components/home/Header';
+import React, { useEffect } from 'react'
+import cookie from 'js-cookie'
+import { useInView } from 'react-intersection-observer'
+import dynamic from 'next/dynamic'
+import Header from './../components/home/Header'
+import prisma from '../lib/prisma'
 
-const WWOContent = dynamic(
-  () => import('../components/home/whatweoffer/WWOContent')
-);
+const WWOContent = dynamic(() => import('../components/home/whatweoffer/WWOContent'))
 
-const Index = () => {
-  const { ref, inView, entry } = useInView({
-    /* Optional options */
-    threshold: 0,
-    triggerOnce: true,
-  });
+const Index = ({
+	testimonials
+}: {
+	testimonials: {
+		id: string
+		name: string
+		image: string
+		message: string
+	}[]
+}) => {
+	const { ref, inView, entry } = useInView({
+		/* Optional options */
+		threshold: 0,
+		triggerOnce: true
+	})
 
-  const checkCookie = async () => {
-    if (cookie.get('OrderDone') !== (undefined || null)) {
-      cookie.remove('OrderDone');
-    } else {
-      return;
-    }
-    return;
-  };
+	const checkCookie = async () => {
+		if (cookie.get('OrderDone') !== (undefined || null)) {
+			cookie.remove('OrderDone')
+		} else {
+			return
+		}
+		return
+	}
 
-  useEffect(() => {
-    checkCookie();
-  }, []);
+	useEffect(() => {
+		checkCookie()
+	}, [])
 
-  const { data, loading, error } = useQuery(ALL_INSTAGRAM);
+	return (
+		<div className="overflow-hidden bg-gradient-to-r from-pink-400 via-green-300 to-blue-400">
+			<Header />
+			<div ref={ref} className="flex w-screen items-center justify-center bg-[#f7f7f5] py-6 lg:py-16 ">
+				<WWOContent testimonials={testimonials} inView={inView} />
+			</div>
+		</div>
+	)
+}
 
-  if (loading) return <Loading />;
-  if (error) return alert(error.message);
+export default Index
 
-  return (
-      <div className='overflow-hidden bg-gradient-to-r from-pink-400 via-green-300 to-blue-400'>
-        <Header />
-        <div
-          ref={ref}
-          className='flex w-screen items-center justify-center bg-[#f7f7f5] py-6 lg:py-16 '
-        >
-          <WWOContent data={data} inView={inView} />
-        </div>
-      </div>
-  );
-};
-
-export default Index;
+export const getStaticProps = async () => {
+	const testimonials = await prisma.testimonials.findMany()
+	return {
+		props: {
+			testimonials
+		}
+	}
+}
