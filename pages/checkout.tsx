1a228d42f056bda3eaf4fda453b1a9ee99921153
kalebslkinch <kalebslkinch@gmail.com>
2022-11-11 00:35:02 +0000

    Added types and comments for checkout page

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -1,13 +1,14 @@
 import { loadStripe } from '@stripe/stripe-js';
 import { Elements } from '@stripe/react-stripe-js';
 import CheckoutForm from '../components/checkout/CheckoutForm';
+import { NextPage } from 'next';
 
 // Make sure to call loadStripe outside of a component’s render to avoid
 // recreating the Stripe object on every render.
 // loadStripe is initialized with your real test publishable API key.
 const promise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
 
-const Checkout = () => {
+const Checkout: NextPage = () => {
   return (
     <div className='App'>
       <Elements stripe={promise}>
