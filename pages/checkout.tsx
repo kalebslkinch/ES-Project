415f07ec025b51278f33ce0327a06ddb6a4b857b
kalebslkinch <kalebslkinch@gmail.com>
2022-11-12 15:39:13 +0000

    Removed logic and imports from Billing

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -1,21 +1,21 @@
-import { loadStripe } from '@stripe/stripe-js';
-import { Elements } from '@stripe/react-stripe-js';
-import CheckoutForm from '../components/checkout/CheckoutForm';
-import { NextPage } from 'next';
+// import { loadStripe } from '@stripe/stripe-js'
+// import { Elements } from '@stripe/react-stripe-js';
+import CheckoutForm from '../components/checkout/CheckoutForm'
+import { NextPage } from 'next'
 
 // Make sure to call loadStripe outside of a component’s render to avoid
 // recreating the Stripe object on every render.
 // loadStripe is initialized with your real test publishable API key.
-const promise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
+// const promise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
 
 const Checkout: NextPage = () => {
-  return (
-    <div className='App'>
-      <Elements stripe={promise}>
-        <CheckoutForm />
-      </Elements>
-    </div>
-  );
-};
+	return (
+		<div className="App">
+			{/* // <Elements stripe={promise}>  */}
+			<CheckoutForm />
+			{/* </Elements> */}
+		</div>
+	)
+}
 
-export default Checkout;
+export default Checkout
