2d3f8d1b17f1456fce8556d82e80ede556d759d6
kalebslkinch <kalebslkinch@gmail.com>
2022-11-16 08:32:36 +0000

    Added correct titles

--- __tests__/order.test.ts
+++ __tests__/order.test.ts
@@ -1,22 +1,7 @@
 import '@testing-library/jest-dom'
 
 describe('Order', () => {
-	const stringProducts: string = JSON.stringify([
-		{
-			id: '2',
-			name: 'Product 2',
-			price: 20,
-			quantity: 10
-		},
-		{
-			id: '3',
-			name: 'Product 3',
-			price: 30,
-			quantity: 10
-		}
-	])
-
-	const productData: {
+	const products: {
 		id: string
 		name: string
 		price: number
@@ -42,33 +27,14 @@ describe('Order', () => {
 		}
 	]
 
-	const products: typeof productData = JSON.parse(stringProducts)
-
-	let filteredWithCorrectQuantity: any[] = []
-	let actualAmounts: number[] = []
-
-	const IDs = products.map((product: { id: string }) => product.id)
-	const filteredProducts = productData.filter((product: { id: string }) => IDs.includes(product.id))
-
-	for (let i = 0; i < filteredProducts.length; i++) {
-		filteredWithCorrectQuantity.push({
-			...filteredProducts[i],
-			quantity: products[i].quantity
-		})
-	}
-
-	filteredWithCorrectQuantity.forEach((each: any) => {
-		actualAmounts.push(each.price * each.quantity)
-	})
-
 	it('total amount of order', () => {
-		const totalAmount = actualAmounts.reduce((a: number, b: number) => a + b, 0)
+		const totalAmount = products.map(product => Number(product.price) * product.quantity).reduce((a, b) => a + b, 0)
 
-		expect(totalAmount).toBe(500)
+		expect(totalAmount).toBe(600)
 	})
 
 	it('total quantity of order', () => {
 		const totalQuantity = products.map(product => product.quantity).reduce((a, b) => a + b, 0)
-		expect(totalQuantity).toBe(20)
+		expect(totalQuantity).toBe(30)
 	})
 })
