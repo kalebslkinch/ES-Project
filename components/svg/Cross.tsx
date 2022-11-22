63ec078709b7f228012058450de973f23f8032dd
kalebslkinch <kalebslkinch@gmail.com>
2022-11-22 23:50:08 +0000

    Added type

--- components/svg/Cross.tsx
+++ components/svg/Cross.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react'
 
-const Cross: FC<{ className }> = ({ className: string }) => {
+const Cross: FC<{ className: string }> = ({ className }) => {
 	return (
 		<>
 			<svg xmlns="http://www.w3.org/2000/svg" className={className} fill="none" viewBox="0 0 24 24" stroke="#4A5568">
