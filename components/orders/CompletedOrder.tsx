fce8139aa36e7de4ef3a06e3a9c0a3914a32b59a
kalebslkinch <kalebslkinch@gmail.com>
2022-11-22 23:09:39 +0000

    Added types

--- components/orders/CompletedOrder.tsx
+++ components/orders/CompletedOrder.tsx
@@ -1,42 +1,29 @@
-import cookie from 'js-cookie';
-import { FC } from 'react';
+import cookie from 'js-cookie'
+import { FC } from 'react'
 
-import { CustomerInfo } from '../../utils/types/customerinfo';
+const CompletedOrder: FC<{
+	quantityAmount: number
+	totalAmount: number
+	payerID: string
+	paypalEmail: string
+	customerInfo: { firstName: string; surName: string; customerEmail: string }
+	paypalInfo: any
+}> = ({ quantityAmount, totalAmount, payerID, paypalEmail, customerInfo, paypalInfo }) => {
+	const { firstName, surName, customerEmail } = customerInfo
 
-interface CompletedOrderProps {
-  customerInfo: CustomerInfo;
-  quantityAmount: string;
-  totalAmount: number;
-  payerID: string;
-  paypalEmail: string;
-  paypalInfo?: String;
+	cookie.remove('cart')
+
+	return (
+		<div className="align-items-center min-w-4xl black flex flex-col border">
+			{firstName} {surName}
+			<h1 className="">You order is now Complete!</h1>
+			<h3>Customer Email: {customerEmail}</h3>
+			<h3>Total Quantity: {quantityAmount}</h3>
+			<h3>Total Cost: £{totalAmount.toFixed(2)}</h3>
+			<h3>Paypal Payer ID: {payerID}</h3>
+			<h3>Paypal Email: {paypalEmail}</h3>
+		</div>
+	)
 }
 
-const CompletedOrder: FC<CompletedOrderProps> = ({
-  quantityAmount,
-  totalAmount,
-  payerID,
-  paypalEmail,
-  customerInfo,
-  paypalInfo,
-}) => {
-  
-
-  const { firstName, surName, customerEmail } = customerInfo;
-
-  cookie.remove('cart');
-
-  return (
-    <div className='align-items-center min-w-4xl black flex flex-col border'>
-      {firstName} {surName}
-      <h1 className=''>You order is now Complete!</h1>
-      <h3>Customer Email: {customerEmail}</h3>
-      <h3>Total Quantity: {quantityAmount}</h3>
-      <h3>Total Cost: £{totalAmount.toFixed(2)}</h3>
-      <h3>Paypal Payer ID: {payerID}</h3>
-      <h3>Paypal Email: {paypalEmail}</h3>
-    </div>
-  );
-};
-
-export default CompletedOrder;
+export default CompletedOrder
