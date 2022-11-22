237605755b07d370b1344d2577075c47762a3ac0
kalebslkinch <kalebslkinch@gmail.com>
2022-11-22 19:35:35 +0000

    Added types to Buttons

--- components/EasyComponents/Buttons.tsx
+++ components/EasyComponents/Buttons.tsx
@@ -1,12 +1,11 @@
-const Button = ({ text, handleClick }) => {
-  return (
-    <button
-      onClick={handleClick}
-      className='mx-auto rounded-lg border border-white bg-gray-600 py-2 px-3'
-    >
-      {text}
-    </button>
-  );
-};
+import { FC } from 'react'
 
-export default Button;
+const Button: FC<{ text: string; handleClick: () => void }> = ({ text, handleClick }) => {
+	return (
+		<button onClick={handleClick} className="mx-auto rounded-lg border border-white bg-gray-600 py-2 px-3">
+			{text}
+		</button>
+	)
+}
+
+export default Button
