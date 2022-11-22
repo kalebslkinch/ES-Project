1da12b0fd4e7754be2d509eedf0b8f5210ed50de
kalebslkinch <kalebslkinch@gmail.com>
2022-11-22 20:07:05 +0000

    Added type

--- components/home/Percentage.tsx
+++ components/home/Percentage.tsx
@@ -1,8 +1,6 @@
-const Percentage = ({ percentage }) => {
-  return (
-    <p className='fixed text-5xl'>
-      {`Percentage scrolled: ${percentage.toPrecision(2)}`}{' '}
-    </p>
-  );
-};
-export default Percentage;
+import { FC } from 'react'
+
+const Percentage: FC<{ percentage: number }> = ({ percentage }) => {
+	return <p className="fixed text-5xl">{`Percentage scrolled: ${percentage.toPrecision(2)}`} </p>
+}
+export default Percentage
