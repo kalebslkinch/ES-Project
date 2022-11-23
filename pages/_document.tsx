670121e77acf1650f3535494ef6cbd4d2e05f037
kalebslkinch <kalebslkinch@gmail.com>
2022-11-23 01:05:52 +0000

    Added type to document file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,27 +1,24 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
-  }
+	static async getInitialProps(ctx: any) {
+		const initialProps = await Document.getInitialProps(ctx)
+		return { ...initialProps }
+	}
 
-  render() {
-    return (
-      <Html lang='en'>
-        <Head>
-          <link
-            href='https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap'
-            rel='stylesheet'
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+	render() {
+		return (
+			<Html lang="en">
+				<Head>
+					<link href="https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap" rel="stylesheet" />
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		)
+	}
 }
 
-export default MyDocument;
+export default MyDocument
