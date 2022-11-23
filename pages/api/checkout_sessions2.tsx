1f34ac4f08e4a1c979097d9302b592ff82a36620
kalebslkinch <kalebslkinch@gmail.com>
2022-11-23 01:10:30 +0000

    Added types

--- pages/api/checkout_sessions2.tsx
+++ pages/api/checkout_sessions2.tsx
@@ -1,38 +1,38 @@
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+import { NextApiRequest, NextApiResponse } from 'next'
 
-export default async function handler(req, res) {
-  if (req.method === "POST") {
-    try {
-      // Create Checkout Sessions from body params.
-      const session = await stripe.checkout.sessions.create({
-        payment_method_types: ["card"],
-        // TODO: replace line_items with your products here
-        line_items: [
-          {
-            price_data: {
-              currency: "gbp",
-              product_data: {
-                name: "Family Order",
-                images: [
-                  `exoticsnaxx-kalebslkinch.vercel.app/public/Exotic_snax.jpg`
-                ]
-              },
-              unit_amount: 6500
-            },
-            quantity: 1
-          }
-        ],
-        mode: "payment",
-        success_url: `${req.headers.origin}/snackbox-order?success=true&session_id={CHECKOUT_SESSION_ID}`,
-        cancel_url: `${req.headers.origin}/snackbox-order?canceled=true`
-      });
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
-      res.redirect(303, session.url);
-    } catch (err) {
-      res.status(500).json({ statusCode: 500, message: err.message });
-    }
-  } else {
-    res.setHeader("Allow", "POST");
-    res.status(405).end("Method Not Allowed");
-  }
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+	if (req.method === 'POST') {
+		try {
+			// Create Checkout Sessions from body params.
+			const session = await stripe.checkout.sessions.create({
+				payment_method_types: ['card'],
+				// TODO: replace line_items with your products here
+				line_items: [
+					{
+						price_data: {
+							currency: 'gbp',
+							product_data: {
+								name: 'Family Order',
+								images: [`exoticsnaxx-kalebslkinch.vercel.app/public/Exotic_snax.jpg`]
+							},
+							unit_amount: 6500
+						},
+						quantity: 1
+					}
+				],
+				mode: 'payment',
+				success_url: `${req.headers.origin}/snackbox-order?success=true&session_id={CHECKOUT_SESSION_ID}`,
+				cancel_url: `${req.headers.origin}/snackbox-order?canceled=true`
+			})
+
+			res.redirect(303, session.url)
+		} catch (err: any) {
+			res.status(500).json({ statusCode: 500, message: err.message })
+		}
+	} else {
+		res.setHeader('Allow', 'POST')
+		res.status(405).end('Method Not Allowed')
+	}
 }
